8f775fd78fe34614bceb544f1058fb65b487aa04
Sajan Nepali <sajanproject@gmail.com>
2025-10-10 19:32:53 +0545

    formik and yup installed

--- src/apps/login.jsx
+++ src/apps/login.jsx
@@ -1,42 +1,63 @@
-import { useState } from "react";
+import { useFormik } from "formik";
+import { object, string } from "yup";
+
+//validation
+
+//yup alternative zod
+//formik alternative react-hook-form
+
+const loginValidation = object({
+  username: string("Username must be string")
+    .min(3, "Username must be 3 character")
+    .max(10)
+    .required("Username is required"),
+  password: string("Password must be string")
+    .min(6, "Minimum length is 6")
+    .required("Password is required"),
+});
 
 function Login() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const { values, handleChange, handleSubmit, errors } = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    validationSchema: loginValidation,
+    onSubmit: (values) => {
+      if (values.username === "sajan" && values.password === "Test@123") {
+        alert("Login Successful");
+      } else {
+        alert("Login Failed");
+      }
+    },
+  });
 
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
-  };
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
-
-  const login = () => {
-    if (username === "sajan" && password === "Test@123") {
-      alert("Login Successful");
-    } else {
-      alert("Login Failed");
-    }
-  };
   return (
-    <div>
+    <form onSubmit={handleSubmit}>
       <h1>Login</h1>
+
       <input
-        value={username}
-        onChange={handleUsernameChange}
+        value={values.username}
+        onChange={handleChange}
         type="text"
         className="border"
         placeholder="Username"
+        name="username"
       />
+      <p className="text-red-400">{errors.username}</p>
       <input
-        value={password}
-        onChange={handlePasswordChange}
+        value={values.password}
+        onChange={handleChange}
         type="password"
         className="border"
         placeholder="Password"
+        minLength={6}
+        required
+        name="password"
       />
-      <button onClick={login}>Login</button>
-    </div>
+      <p className="text-red-400">{errors.password}</p>
+      <button type="submit">Login</button>
+    </form>
   );
 }
 export default Login;
